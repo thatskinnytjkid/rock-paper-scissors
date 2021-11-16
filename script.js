3b1497b8a704b8f18f40370dab7f3ec729c6b55a
TJ Martinez <tjmart1074@gmail.com>
2021-11-16 12:30:59 -0600

    Add rematch option and cleaned up interface

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
+const buttons = document.querySelectorAll('button');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const scoreDisplay = document.getElementById('playerScore');
+const computerDisplay = document.getElementById('computerScore');
+const previousRound = document.getElementById('previousRound');
+const rematchButton = document.getElementById('rematch');
 
-//Rock Paper Scissors
-
-let options = ['Rock', 'Paper', 'Scissors'];
-let playerScore = 0;
-let computerScore = 0;
 
 //Computer Choice - Randomize
 function computerPlay(){
+    let options = ['Rock', 'Paper', 'Scissors'];
     let random = Math.floor(Math.random() * 3);
     return options[random].toLowerCase();
 }
 
-// Player Choice - Button Listeners
-function getPlayerChoice(e){
-    
-}
 
-//Play Round - Compare
-function playRound(playerChoice, computerChoice){
+//Play Round - Compare, Add Scores, and Display Result
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerChoice){
 
     computerChoice = computerPlay();
-    //console.log(computerChoice);
-    playerChoice = (prompt('Rock, Paper, or Scissors?')).toLowerCase();
+    console.log(`Player: ${playerChoice}`);
+    console.log(`Computer: ${computerChoice}`);
     if (computerChoice === playerChoice){
-        console.log("It's a tie!");
+        previousRound.textContent = "It's a tie!";
     }
     else if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-        console.log("You're the winner!");
+        previousRound.textContent = "Nice one!";
         playerScore += 1;
+        scoreDisplay.textContent = 'Your Score: ' + playerScore;
     } else {
-        console.log("You suck. You lost");
+        previousRound.textContent = "You suck...";
         computerScore += 1;
+        computerDisplay.textContent = 'Opponent: ' + computerScore;
     }
-}
-
-// 5 Round Game
-function game(){
-    for (i = 0; i < 5; i++){
-        playRound();
+    if ((playerScore === 5) || (computerScore === 5)){
+        checkScore(playerScore, computerScore);
     }
-    console.log(`You won ${playerScore} rounds, and lost ${computerScore} rounds.`);
 
 }
 
+// Score checker / Disable buttons / Enable Rematch button
+function checkScore(playerScore, computerScore){
+    if (playerScore === 5) {
+        previousRound.textContent = "You've won the match!";
+    } else if (computerScore === 5) {
+        previousRound.textContent = "GOD YOU'RE SO STUPID!!!";
+    } 
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = true;
+    }
+    rematchButton.hidden = false;
+    rematchButton.disabled = false;
 
-// game();
+}
+
+rematchButton.addEventListener('click', rematch);
+
+function rematch(){
+    playerScore = computerScore = 0;
+    scoreDisplay.textContent = 'Your Score: ' + playerScore;
+    computerDisplay.textContent = 'Opponent: ' + computerScore;
+    previousRound.textContent = "Choose wisely...";
+    rematchButton.hidden = true;
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = false;
+    };
+};
+
+// Listen for click and trigger playRound(players choice)
+buttons.forEach(button => button.addEventListener('click', () => {
+    if (button.id === 'rock') {
+        playRound('rock');
+    } else if (button.id === 'paper'){
+        playRound('paper');
+    } else if (button.id === 'scissors'){
+        playRound('scissors');
+    }
+}));
